b92690faafd6053aae719457770ff7f3f66192a2
gar-mart <garmartirosy@gmail.com>
2026-01-17 01:09:35 -0500

    Add project files.

---
+++ Personal Finance Manager/ViewModels/CreateAccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Personal_Finance_Manager.ViewModels
+{
+    public class CreateAccountViewModel
+    {
+        public string Name { get; set; }
+
+        public int AccountTypeId { get; set; }
+
+        public decimal Balance { get; set; }
+
+    }
+}
---
+++ Personal Finance Manager/ViewModels/CreateTransactionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Personal_Finance_Manager.ViewModels
+{
+    public class CreateTransactionViewModel
+    {
+        public int AccountId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public int TransactionTypeId { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+        
+        public string? DestinationName { get; set; }
+        public string? Description { get; set; }
+
+        public int? CategoryId { get; set; }
+    }
+}
